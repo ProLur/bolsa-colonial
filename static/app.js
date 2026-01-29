d1f13e058c38338c4bf78105c6556c8f139a3a5c
ProLur <mmfrutos@outlook.com>
2026-01-29 19:39:11 +0100

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,1 +1,21 @@
+const API = "";
 
+async function loadMarket() {
+    const res = await fetch("/market");
+    const data = await res.json();
+    document.getElementById("price").innerText =
+        data.price.toFixed(2) + " €";
+}
+
+async function buy(amount) {
+    await fetch(`/buy?amount=${amount}`);
+    loadMarket();
+}
+
+async function sell(amount) {
+    await fetch(`/sell?amount=${amount}`);
+    loadMarket();
+}
+
+loadMarket();
+setInterval(loadMarket, 5000);
